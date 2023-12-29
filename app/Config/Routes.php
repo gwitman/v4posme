7834f477838393a7a25647104a80eb7169a57008
Witman Gonzalez <gwitman>
2023-12-28 20:56:41 -0600

    posMe**racncho pura vida

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -774,6 +774,8 @@ $routes->match(['get','post'],'app_invoice_billing/viewRegisterInstitutoLaVid','
 $routes->match(['get','post'],'app_invoice_billing/viewRegisterInstitutoLaVid/(:any)','app_invoice_billing::viewRegisterInstitutoLaVid');
 $routes->match(['get','post'],'app_invoice_billing/viewPrinterDirectFactura80mm','app_invoice_billing::viewPrinterDirectFactura80mm');
 $routes->match(['get','post'],'app_invoice_billing/viewPrinterDirectFactura80mm/(:any)','app_invoice_billing::viewPrinterDirectFactura80mm');
+$routes->match(['get','post'],'app_invoice_billing/viewPrinterDirectFactura80mmPuraVida','app_invoice_billing::viewPrinterDirectFactura80mmPuraVida');
+$routes->match(['get','post'],'app_invoice_billing/viewPrinterDirectFactura80mmPuraVida/(:any)','app_invoice_billing::viewPrinterDirectFactura80mmPuraVida');
 $routes->match(['get','post'],'app_invoice_billing/viewPrinterDirectFactura80mmRustikChillGrill','app_invoice_billing::viewPrinterDirectFactura80mmRustikChillGrill');
 $routes->match(['get','post'],'app_invoice_billing/viewPrinterDirectFactura80mmRustikChillGrill/(:any)','app_invoice_billing::viewPrinterDirectFactura80mmRustikChillGrill');
 $routes->match(['get','post'],'app_invoice_billing/viewPrinterDirectFactura80mmFerreteriaDouglas','app_invoice_billing::viewPrinterDirectFactura80mmFerreteriaDouglas');
--- public/resource/file_sql/actualizar_parametro_rancho_pura_vida.sql
+++ public/resource/file_sql/actualizar_parametro_rancho_pura_vida.sql
@@ -612,7 +612,7 @@ WHERE
 	
 				
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "app_invoice_billing/viewPrinterDirectFactura80mm" 
+	tb_company_parameter.value = "app_invoice_billing/viewPrinterDirectFactura80mmPuraVida" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "INVOICE_BILLING_PRINTER_DIRECT_URL";## Urls para imprimir el recibo directamente en la impresora, mejor dicho imprimir la factura 
