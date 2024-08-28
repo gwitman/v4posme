2267bca3170cc3db1013f7916a77be88bb7c8aa7
gwitman <gwitman@yahoo.com>
2024-08-28 11:19:29 -0600

    commit hiper all

--- public/resource/file_sql/actualizar_parametro_hiperall.sql
+++ public/resource/file_sql/actualizar_parametro_hiperall.sql
@@ -1292,7 +1292,7 @@ WHERE
 				
 				
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "2024-08-25" 
+	tb_company_parameter.value = "2050-08-25" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "CORE_CUST_PRICE_LICENCES_EXPIRED";## Fecha de expiracion de la licencia 
