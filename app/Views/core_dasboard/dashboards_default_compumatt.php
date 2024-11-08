ab6bcae78826d668cfd7d0dcd4956f62f30d6830
gwitman <gwitman@yahoo.com>
2024-11-08 12:37:22 -0600

    cambios en diseño de grid

--- app/Views/core_dasboard/dashboards_default_compumatt.php
+++ app/Views/core_dasboard/dashboards_default_compumatt.php
@@ -78,17 +78,19 @@
 				</div>
 				
 				<div class="row">
-					<div class="panel" style="margin-bottom:20px;">
-						<div class="panel-heading">
-							<div class="icon"><i class="icon20 i-health"></i></div> 
-							<h4>Colaborador vs Estados de equipos</h4>
-							<a href="#" class="minimize"></a>
-						</div><!-- End .panel-heading -->
-						
-						<div class="panel-body">								
-							<div id="grafico1" style="height:300px" ></div>
-						</div><!-- End .panel-body -->
-					</div><!-- End .widget -->
+					<div class="col-lg-12">
+						<div class="panel" style="margin-bottom:20px;">
+							<div class="panel-heading">
+								<div class="icon"><i class="icon20 i-health"></i></div> 
+								<h4>Colaborador vs Estados de equipos</h4>
+								<a href="#" class="minimize"></a>
+							</div><!-- End .panel-heading -->
+							
+							<div class="panel-body">								
+								<div id="grafico1" style="height:300px" ></div>
+							</div><!-- End .panel-body -->
+						</div><!-- End .widget -->
+					</div>
 				</div>
 				
 				
