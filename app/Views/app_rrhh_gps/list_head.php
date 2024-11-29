00e5bca6e4f0de2356f700b5829e4ac7293b17fa
LaelMedina <medinalael10@gmail.com>
2024-11-29 10:52:01 -0600

    feat(view): Adding the google maps view

---
+++ app/Views/app_rrhh_gps/list_head.php
@@ -0,0 +1,10 @@
+<link href="<?php echo APP_URL_RESOURCE_CSS_JS; ?>/resource/theme-genyx/js/plugins/forms/datepicker/datepicker.css" rel="stylesheet" />
+<link href="<?php echo APP_URL_RESOURCE_CSS_JS; ?>/resource/js/is-loading-master/style.css" rel="stylesheet" />
+<script src="<?php echo APP_URL_RESOURCE_CSS_JS; ?>/resource/js/is-loading-master/jquery.isloading.min.js"></script>
+<script src="<?php echo APP_URL_RESOURCE_CSS_JS; ?>/resource/js/genyx-fn.js"></script>
+<script src="<?php echo APP_URL_RESOURCE_CSS_JS; ?>/resource/js/chart-google/loader.js"></script>
+<script src="<?php echo APP_URL_RESOURCE_CSS_JS; ?>/resource/js/jLinq-2.2.1.js"></script>
+<script src="<?php echo APP_URL_RESOURCE_CSS_JS; ?>/resource/js/jquery.number.min.js"></script>
+
+<!-- Link API Google Maps (PosMe API Key) -->
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDV_kkjf0B6tCsFekM2x3wcqacvGm4ASx0&loading=async&callback=fnCargarMapa"></script>
