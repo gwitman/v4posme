9916a584457d09ce47257fe50834c5656257326d
gwitman <gwitman@yahoo.com>
2025-01-27 23:21:26 -0600

    cambios de emnauel

--- public/resource/file_sql/actualizar_parametro_enmanuel.sql
+++ public/resource/file_sql/actualizar_parametro_enmanuel.sql
@@ -535,7 +535,7 @@ WHERE
 	
 	
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "" 
+	tb_company_parameter.value = "---" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "CORE_LABEL_SISTEMA_SUPLANTATION";## Se usa para poner un label al sistema,  como un segundo nombre de sistema
