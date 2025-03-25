047f63ab1047751be5f8847d3fd6c6924b62ece9
gwitman <gwitman@yahoo.com>
2025-03-25 16:48:06 -0600

    actualiza tipo de cambio

--- public/resource/file_sql/actualizar_parametro_pasteleria_lizzette.sql
+++ public/resource/file_sql/actualizar_parametro_pasteleria_lizzette.sql
@@ -1733,7 +1733,7 @@ update tb_role set isActive = 1 where roleID in (
 
 /*tipo de cambio de dolares a cordoba*/
 update tb_exchange_rate set 
-	ratio = 36 
+	ratio = 37
 where 
 	currencyID = 2
 	and targetCurrencyID = 1; 
@@ -1741,7 +1741,7 @@ where
 	
 /*tipo de cambio de dolares a cordoba*/
 update tb_exchange_rate set 
-	ratio = 0.027777 
+	ratio = 0.027027027027027
 where 
 	currencyID = 1
 	and targetCurrencyID = 2; 
