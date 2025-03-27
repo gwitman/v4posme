e03b173f7df9bdcbfc49167560f9d11c1cc33a06
gwitman <gwitman@yahoo.com>
2025-03-26 18:22:21 -0600

    personalizar pastleria

--- app/Helpers/customization/web_tools_customizationview_pasteleriaballadares_helper.php
+++ app/Helpers/customization/web_tools_customizationview_pasteleriaballadares_helper.php
@@ -4,7 +4,12 @@ function getBehavioPasteleriaBalladares(): array
 {
     //company type: maximum
     return array(
-		strtolower('pasteleria_balladares_app_cxc_customer_divScriptReady')         		=> "$('#txtIdentification').val('".date("Y_m_d_h_i")."');"
+		strtolower('pasteleria_balladares_app_cxc_customer_divScriptReady')         		=> "$('#txtIdentification').val('".date("Y_m_d_h_i")."');",
+		strtolower('pasteleria_balladares_app_invoice_billing_txtTraductionMesa')         	=> "Entregado",
+		strtolower('pasteleria_balladares_app_invoice_billing_divLabelZone')         		=> "Hora",
+		strtolower('pasteleria_balladares_app_invoice_billing_divPrecio')         			=> "hidden",
+		strtolower('pasteleria_balladares_app_invoice_billing_divBodegaHidden')         	=> "hidden",
+		strtolower('pasteleria_balladares_app_invoice_billing_divMesa')			         	=> "hidden",
     );
 }
 
