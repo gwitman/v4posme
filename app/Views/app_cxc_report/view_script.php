290e84e5f3b51e13b6c7c412d2ba16a18d7c7348
gwitman <gwitman@yahoo.com>
2025-07-06 21:13:54 -0600

    paginas del nuevo formato de reportes

--- app/Views/app_cxc_report/view_script.php
+++ app/Views/app_cxc_report/view_script.php
@@ -1,2 +1,11 @@
-				<!-- ./ page heading -->
-				
+<!-- ./ page heading -->
+<script>
+document.querySelectorAll('.reporte-item').forEach(item => {
+  item.addEventListener('click', () => {
+	const url = item.getAttribute('data-url');
+	if (url) {
+	  window.open(url, '_blank');
+	}
+  });
+});
+</script>
