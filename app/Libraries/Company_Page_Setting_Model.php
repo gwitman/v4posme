3639af05e21c479dac57e2e6e241c25819bb6ceb
jose miguel romero sevilla <jmiguelsevilla@hotmail.com>
2025-07-09 12:04:36 -0600

    modelo de company setting page e imagenes

---
+++ app/Libraries/Company_Page_Setting_Model.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Libraries;
+
+use CodeIgniter\Model;
+use ReflectionException;
+
+class Company_Page_Setting_Model extends Model
+{
+    protected $table            = "tb_company_page_setting";
+    protected $primaryKey       = 'customPageID';
+    protected $useAutoIncrement = true;
+    protected $returnType       = 'object';
+
+    public function insert_app_posme($data){
+        try {
+            return $this->insert($data);
+        } catch (ReflectionException $e) {
+            return 0;
+        }
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function delete_app_posme($customPageID){
+        $this->where('customPageID', $customPageID)->set(['isActive' => 0])->update();
+    }
+
+    /**
+     * @throws ReflectionException
+     */
+    public function update_app_posme($customPageID, $data){
+        $this->where('customPageID', $customPageID)->set($data)->update();
+    }
+
+    public function get_rowByPk($customPageID){
+        return $this->where(['customPageID' => $customPageID, 'isActive'=>1])->first();
+    }
+
+    public function get_rowByKey($key){
+        return $this->where(['key' => $key, 'isActive'=>1])->findAll();
+    }
+
+    public function get_rowByKeyAndControllerAndEmelement($key, $controller, $element){
+        return $this->where([
+            'key'           => $key,
+            'controller'    => $controller,
+            'element'       => $element,
+            'isActive'      =>1
+        ])->first();
+    }
+}
