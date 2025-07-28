22bacc618092af1084c0cb9d0eb20cad51a94611
gwitman <gwitman@yahoo.com>
2025-07-28 07:38:18 -0600

    comercial san jose

--- public/resource/file_sql/actualizar_parametro_comercial_san_jose.sql
+++ public/resource/file_sql/actualizar_parametro_comercial_san_jose.sql
@@ -79,7 +79,7 @@ WHERE
 	
 	
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "true" 
+	tb_company_parameter.value = "false" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "CORE_TRAKING_GPS";## Valida si es neceasrio llevar el seguimiento del gps
