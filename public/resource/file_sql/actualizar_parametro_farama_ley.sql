d55299b9cc84412da0dc1cb9cc9bb63efbd97eb2
Delvis Cisneros <delvis.cisneros12@est.unanleon.edu.ni>
2025-08-28 12:28:42 -0600

    #TAS00000338 cambio de archivo de configuracion farma ley

--- public/resource/file_sql/actualizar_parametro_farama_ley.sql
+++ public/resource/file_sql/actualizar_parametro_farama_ley.sql
@@ -744,7 +744,7 @@ WHERE
 	tb_parameter.name = "CXC_PLAZO_DEFAULT";## Plazo predeterminado
 	
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "true" 
+	tb_company_parameter.value = "false" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "SHARE_INVOICE_BY_INVOICE";## Si este valor esta en true, se tiene que seleccionar una factura a la ves, par ir abonando una a una
