3d6bf8bfbc28d15c2a7495240fdf29904bd68686
gwitman <gwitman@yahoo.com>
2025-11-06 07:29:04 -0600

    actualizar parametros del barro

--- public/resource/file_sql/actualizar_parametro_jorge_ramirez_el_barro.sql
+++ public/resource/file_sql/actualizar_parametro_jorge_ramirez_el_barro.sql
@@ -1626,7 +1626,7 @@ WHERE
 
 
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "true" 
+	tb_company_parameter.value = "false" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "INVOICE_PARAMTER_AMORITZATION_DURAN_INVOICE";## Aplicar tabla de amortizacoin segun parametros durante la facturacion.
