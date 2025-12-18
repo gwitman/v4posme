9cdc1e8fed1b96edb589dd75263ac651e87ed5c7
gwitman <gwitman@yahoo.com>
2025-12-18 16:48:34 -0600

    cambios

--- public/resource/file_sql/actualizar_parametro_public.sql
+++ public/resource/file_sql/actualizar_parametro_public.sql
@@ -1062,7 +1062,7 @@ WHERE
 				
 				
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "2024-03-07" 
+	tb_company_parameter.value = "2026-03-07" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "CORE_CUST_PRICE_LICENCES_EXPIRED";## Fecha de expiracion de la licencia 
