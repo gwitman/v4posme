e8c325fc98e117cf31d085103e9d982ab07b83e8
gwitman <gwitman@yahoo.com>
2026-01-14 10:32:40 -0600

    actualizar parametro de de distrito 4199

--- public/resource/file_sql/actualizar_parametro_distrito_4199_malpaisillo.sql
+++ public/resource/file_sql/actualizar_parametro_distrito_4199_malpaisillo.sql
@@ -100,7 +100,7 @@ WHERE
 
 
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "true" 
+	tb_company_parameter.value = "false" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "INVENTORY_INSRT_ALL_WAREHOUSE_IN_NEWITEM"; 
