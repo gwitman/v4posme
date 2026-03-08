53875879530e6d33319afb7e04b9c6183852d7a8
gwitman <gwitman@yahoo.com>
2026-03-07 22:27:56 -0600

    fdf

--- public/resource/file_sql/actualizar_parametro_posme.sql
+++ public/resource/file_sql/actualizar_parametro_posme.sql
@@ -16,7 +16,7 @@ WHERE
 	
 	
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "ec60f0a1-754c-41a9-961f-f1b98fc47929" 
+	tb_company_parameter.value = "8bbb0a92-93e0-4600-ac88-9c97b478edff" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "WHATSAP_URL_REQUEST_SESSION";## Se usa para poner un label al sistema,  como un segundo nombre de sistema
