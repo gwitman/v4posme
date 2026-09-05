0d97cf06bf95286222690b72b918307ca68b6626
gwitman <gwitman@yahoo.com>
2026-09-04 20:17:21 -0600

    actualiar cambios

--- public/resource/file_sql/actualizar_parametro_alpha_dbl_motor.sql
+++ public/resource/file_sql/actualizar_parametro_alpha_dbl_motor.sql
@@ -772,7 +772,7 @@ WHERE
 	tb_parameter.name = "WHATSAP_TOCKEN";## Se usa para poner un label al sistema,  como un segundo nombre de sistema
 	
 UPDATE  tb_company_parameter,tb_parameter SET 
-	tb_company_parameter.value = "Ariel" 
+	tb_company_parameter.value = "alphaDblMotorV001" 
 WHERE 
 	tb_company_parameter.parameterID = tb_parameter.parameterID AND 
 	tb_parameter.name = "WHATSAP_URL_REQUEST_SESSION";## Se usa para poner un label al sistema,  como un segundo nombre de sistema
